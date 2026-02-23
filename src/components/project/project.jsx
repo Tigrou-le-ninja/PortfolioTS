f5ebffe60f6311ac5ce3b4a6b6e315b51558c92a
Théo Sommacal <t.somm@yahoo.fr>
2026-02-23 18:28:30 +0100

    Reformattage des images

--- src/components/project/project.jsx
+++ src/components/project/project.jsx
@@ -8,7 +8,7 @@ const Project = ({ project }) => {
   return (
     <div className="project">
       <h2>{project.title}</h2>
-      {/* <Carousel pictures={project.pictures} /> */}
+      <Carousel pictures={project.pictures} />
       <p>{t(`projectsContent.${project.id}`)}</p>
     </div>
   );
