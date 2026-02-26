5c3d8e169cbc8bd35307d551a6495fbad0fdaf97
Théo Sommacal <t.somm@yahoo.fr>
2026-02-26 15:25:01 +0100

    Fin du styling

--- src/components/themeswitch/themeswitch.jsx
+++ src/components/themeswitch/themeswitch.jsx
@@ -1,3 +1,11 @@
+// FontAwesome Icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+library.add(fas, far, fab);
+
 import { useState } from "react";
 
 import "./themeswitch.scss";
@@ -12,16 +20,26 @@ const ThemeSwitch = () => {
     rootTheme.setAttribute("data-theme", newTheme);
   };
 
-  // Mettre "transition: all 1s ease;" dans le css des blocks et composants pour un changement de thème fluide!
-
   console.log("Current theme:", theme);
 
   return (
     <>
+      {/* Version PC : toggle */}
       <label className="toggle">
         <input type="checkbox" onChange={toggleTheme} checked={theme === "light"} />
         <span className="slider"></span>
       </label>
+
+      {/* Version Mobile : button qui change le thème au click et dont le contenu change en fonction du thème */}
+      <div className="toggle-mobile">
+        <button onClick={toggleTheme}>
+          {theme === "light" ? (
+            <FontAwesomeIcon icon="fa-regular fa-sun" className="theme-icon-mobile" />
+          ) : (
+            <FontAwesomeIcon icon="fa-regular fa-moon" className="theme-icon-mobile" />
+          )}
+        </button>
+      </div>
     </>
   );
 };
